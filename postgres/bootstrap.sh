1c9306e38a87fd4c2c1f07f541687b38540f47c1
geekduck <duck1218+github@gmail.com>
2015-12-02 19:20:49 +0900

    Delete volume.

--- postgres/bootstrap.sh
+++ postgres/bootstrap.sh
@@ -8,5 +8,4 @@ docker run -d --name $DOCKER_CONTAINER_NAME \
   -e POSTGRES_PASSWORD=postgres \
   -p 5432:5432 \
   -e "TZ=Asia/Tokyo" \
-  -v `pwd`/data:/var/lib/postgresql/data \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG
